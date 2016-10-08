789b0a0a90d70e0336badb5d3e4bd72b4e4e5f73
Maxime Perusse <Maxime Perusse>
2016-10-08 18:32:37 -0400

    done for real

--- PolyQuiz/public/javascripts/scripts.js
+++ PolyQuiz/public/javascripts/scripts.js
@@ -38,21 +38,26 @@ function displayStatistics(modal) {
 
 function displayDetails() {
 	$(document).ready(function() 
-	{
+	{	
 		var examCount = window.localStorage['stats.exams.count'];
 
 		var examTable = document.getElementById('details-exams-table');
+
+		while(examTable.hasChildNodes()){
+	    	examTable.removeChild(examTable.lastChild);
+		}
+		
 		for(var i = 0; i < examCount; i++) {
 			var examCorrect = window.localStorage['stats.exams.' + i + '.correct'];
 			var examTotal = window.localStorage['stats.exams.' + i + '.total'];
 			var examDomain = window.localStorage['stats.exams.' + i + '.domain'];
 			var row = examTable.insertRow();
 
-	        var examTitle = row.insertCell(0);
-	        var examResult = row.insertCell(1);
+			var examTitle = row.insertCell(0);
+			var examResult = row.insertCell(1);
 
-	        examTitle.innerHTML = 'Examen #' + i + ' - ' + examDomain;
-	        examResult.innerHTML = examCorrect + '/' + examTotal;
+			examTitle.innerHTML = 'Examen #' + i + ' - ' + examDomain;
+			examResult.innerHTML = examCorrect + '/' + examTotal;
 		}
 	});
 }
@@ -86,14 +91,87 @@ function saveResults(correct, total, domaine) {
 }
 
 function initResults() {
+	// TODO remove all exams individual results!
+	var count = window.localStorage['stats.exams.count'];
+	for(var i = 0; i < count; i++) {
+		window.localStorage.removeItem('stats.exams.' + i + '.correct');
+		window.localStorage.removeItem('stats.exams.' + i + '.total');
+		window.localStorage.removeItem('stats.exams.' + i + '.percentage');
+		window.localStorage.removeItem('stats.exams.' + i + '.domain');
+	}
+
 	window.localStorage['stats.exams.count'] = 0;
+	window.localStorage['stats.exams.average'] = 0;
+
 	window.localStorage['stats.exams.domain.css.total'] = 0;
 	window.localStorage['stats.exams.domain.html.total'] = 0;
 	window.localStorage['stats.exams.domain.js.total'] = 0;
 	window.localStorage['stats.exams.domain.css.passed'] = 0;
 	window.localStorage['stats.exams.domain.html.passed'] = 0;
 	window.localStorage['stats.exams.domain.js.passed'] = 0;
+
 	window.localStorage['stats.quicktests.correct'] = 0;
 	window.localStorage['stats.quicktests.total'] = 0;
-	window.localStorage['stats.exams.average'] = 0;
+}
+
+// drag and drop
+var correctAnswer;
+
+function setCorrectAnswer(index) {
+	correctAnswer = index;
+}
+
+function handleAnswerDragStart(e) {
+	this.style.opacity = '0.4';
+
+	dragSrcEl = this;
+
+	e.dataTransfer.effectAllowed = 'move';
+	e.dataTransfer.setData('index', $(this).attr('index'));
+	e.dataTransfer.setData('text', $(this).text());
+}
+
+function handleAnswerDragStart(e) {
+	this.style.opacity = '0.4';
+
+	dragSrcEl = this;
+
+	e.dataTransfer.effectAllowed = 'move';
+	e.dataTransfer.setData('index', $(this).attr('index'));
+	e.dataTransfer.setData('text', $(this).text());
+}
+
+function handleAnswerDragEnd(e) {
+	this.style.opacity = '1';
+}
+
+function handleAnswerBoxDragOver(e) {
+	if (e.preventDefault) {
+		e.preventDefault(); // Necessary. Allows us to drop.
+	}
+
+	e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+	return false;
+}
+
+function handleAnswerBoxDragEnter(e) {
+	// this / e.target is the current hover target.
+	this.classList.add('over');
+}
+
+function handleAnswerBoxDragLeave(e) {
+	this.classList.remove('over');  // this / e.target is previous target element.
+}
+
+function resetQuestionStyles() {
+	var answers = $('#answer .possibleAnswer');
+	[].forEach.call(answers, function(answer) {
+		answer.setAttribute('draggable', 'true');
+		answer.style.cursor = 'move';
+	});
+
+	var answerBox = document.querySelector('#answerbox');
+	answerBox.style.borderColor = 'black';
+	answerBox.innerHTML = '';
 }
